bba3aa3b9cc784d0d0ebdd45325dc09901a5492f
Dominik Helleberg <dominik.helleberg@gmail.com>
2015-01-18 10:27:32 +0100

    removed second section

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 #include "pebble.h"
 
-#define NUM_MENU_SECTIONS 2
+#define NUM_MENU_SECTIONS 1
 #define NUM_MENU_ICONS 3
 #define NUM_FIRST_MENU_ITEMS 3
-#define NUM_SECOND_MENU_ITEMS 1
 
 static Window *window;
 
@@ -32,9 +31,6 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
     case 0:
       return NUM_FIRST_MENU_ITEMS;
 
-    case 1:
-      return NUM_SECOND_MENU_ITEMS;
-
     default:
       return 0;
   }
@@ -49,23 +45,13 @@ static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t s
 // Here we draw what each header is
 static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
   // Determine which section we're working with
-  switch (section_index) {
-    case 0:
-      // Draw title text in the section header
-      menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
-      break;
-
-    case 1:
-      menu_cell_basic_header_draw(ctx, cell_layer, "One more");
-      break;
-  }
+  // Draw title text in the section header
+  menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
 }
 
 // This is the menu item draw callback where you specify what each item should look like
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
   // Determine which section we're going to draw in
-  switch (cell_index->section) {
-    case 0:
       // Use the row to specify which item we'll draw
       switch (cell_index->row) {
         case 0:
@@ -85,16 +71,6 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
               (GRect){ .origin = GPointZero, .size = layer_get_frame((Layer*) cell_layer).size });
           break;
       }
-      break;
-
-    case 1:
-      switch (cell_index->row) {
-        case 0:
-          // There is title draw for something more simple than a basic menu item
-          menu_cell_title_draw(ctx, cell_layer, "Final Item");
-          break;
-      }
-  }
 }
 
 // Here we capture when a user selects a menu item
